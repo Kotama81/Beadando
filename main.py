79cdef438486b712327f0e57f8d805db28e98d68
Kotama81 <kotama1981@gmail.com>
2024-06-01 13:35:12 +0200

    a szamitogep random valaszt a harom lehetosegbol, majd kiprinteli azt

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ if korok_szama > 5 or korok_szama < 0 :  #ha nem 1-5 ig irja a szamot akkor figy
     print("Nem jó számot írtál ,írd be újra!") 
     korok_szama = int(input("Hány körből álljon a játék?\nÍrj be egy számot egytől ötig! ")) 
 
+alakzatok = ["kor", "papir", "ollo"]
 kor = 0
 while korok_szama > kor:
     jatekos_valasztas = int(input("Melyiket választod?\n1-es gomb kő\n2-es gomb papír\n3-as gomb olló\n")) #bekerem a jatekostol, hogy melyik format valasztja
@@ -13,10 +14,21 @@ while korok_szama > kor:
         print("Nem jó számot választottál, válassz újra!")
         jatekos_valasztas = int(input("Melyiket választod?\n1-es gomb kő\n2-es gomb papír\n3-as gomb olló\n"))
     
-    if jatekos_valasztas == 1:  #valasztastol fuggoen kiprintelem a kepernyore a valasztott alakzatot
+    print("Játékos választása:") #valasztastol fuggoen kiprintelem a kepernyore a valasztott alakzatot
+    if jatekos_valasztas == 1:  
         print(kezek.ko)
     elif jatekos_valasztas == 2:
         print(kezek.papir)
     else:
         print(kezek.ollo)
     
+    print("Számítógép választása:")  # a szamitogep random valaszt a harom lehetosegbol, majd kiprinteli azt
+    gep_valasztasa = random.choice(alakzatok)
+    if gep_valasztasa == "ko":
+        print(kezek.ko)
+    elif gep_valasztasa == "papir":
+        print(kezek.papir)
+    else:
+        print(kezek.ollo)
+
+
